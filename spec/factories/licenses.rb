d91cf687bfd2293c140cc2115be8922f08a0e46f
Nathan Stitt <nathan@stitt.org>
2018-02-21 09:45:52 -0600

    add validations, make read-only

--- spec/factories/licenses.rb
+++ spec/factories/licenses.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :license do
     abbreviation { ['CC BY', 'CC BY-SA', 'CC BY-ND', 'CC BY-NC'].sample }
     url { Faker::Internet.url }
+    name { Faker::Lorem.words }
     terms { Faker::Lorem.paragraphs }
   end
 end
--- spec/models/license_spec.rb
+++ spec/models/license_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe License, type: :model do
   it "has working factory" do
     FactoryBot.create :license
   end
+
+  it "can't be udpated" do
+    license = FactoryBot.create :license
+    license.terms = 'you hereby agree to reliquish your first-born to the lord of darkness'
+    expect{ license.save }.to raise_error(ActiveRecord::ReadOnlyRecord)
+  end
 end

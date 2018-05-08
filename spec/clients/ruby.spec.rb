1c0bfe6b9fd4281178b32fdcc1d700040ec56dce
Nathan Stitt <nathan@stitt.org>
2018-05-08 15:53:22 -0500

    test fetching assessments and format

--- spec/clients/ruby.spec.rb
+++ spec/clients/ruby.spec.rb
@@ -6,7 +6,6 @@ describe 'Ruby client', type: :api do
 
   let(:user) { FactoryBot.create(:user) }
   let(:format) { FactoryBot.create(:format) }
-  let(:api_instance) { A15kClient::AssessmentsApi.new }
   let(:authorization) { Access::Token.for_user(user) }
 
   before(:each) {
@@ -17,37 +16,75 @@ describe 'Ruby client', type: :api do
     end
   }
 
-  it 'can create an assessment' do
-    response = api_instance.create_assessment(
-      format_id: format.id,
-      identifier: 'TEST-Test-AND-TEST-MORE',
-      content: { text: Faker::Lorem.paragraph }.to_json,
-      questions: 2.times.map{|question_index|
-        {
-          format_id: format.id,
-          content: {
-            index: question_index,
-            text: Faker::Lorem.paragraph
-          }.to_json,
-          solutions: 3.times.map{ |solution_index|
-            {
-              format_id: format.id,
-              content: {
-                index: solution_index, text: Faker::Lorem.paragraph
-              }.to_json,
+  describe 'Assessments' do
+    let(:api_instance) { A15kClient::AssessmentsApi.new }
+
+    it 'can be fetched' do
+      assessment = FactoryBot.create(:assessment, format: format)
+      response = api_instance.get_assessment(
+        assessment.id
+      )
+      expect(response.success).to eq(true)
+      expect(response.data).to be_a_kind_of(A15kClient::Assessment)
+    end
+
+    it 'can be created' do
+      response = api_instance.create_assessment(
+        format_id: format.id,
+        identifier: 'TEST-Test-AND-TEST-MORE',
+        content: { text: Faker::Lorem.paragraph }.to_json,
+        questions: 2.times.map{|question_index|
+          {
+            format_id: format.id,
+            content: {
+              index: question_index,
+              text: Faker::Lorem.paragraph
+            }.to_json,
+            solutions: 3.times.map{ |solution_index|
+              {
+                format_id: format.id,
+                content: {
+                  index: solution_index, text: Faker::Lorem.paragraph
+                }.to_json,
+              }
             }
           }
         }
-      }
-    )
-    expect(response.success).to eq(true)
-    expect(response.data).to be_a_kind_of(A15kClient::Assessment)
-    assessment = response.data
-    expect(assessment.identifier).to eq('TEST-Test-AND-TEST-MORE')
-    expect(assessment.format_id).to eq(format.id)
-    expect(assessment.questions.length).to eq(2)
-    expect(assessment.questions[0].solutions.length).to eq(3)
+      )
+      expect(response.success).to eq(true)
+      expect(response.data).to be_a_kind_of(A15kClient::Assessment)
+      assessment = response.data
+      expect(assessment.identifier).to eq('TEST-Test-AND-TEST-MORE')
+      expect(assessment.format_id).to eq(format.id)
+      expect(assessment.questions.length).to eq(2)
+      expect(assessment.questions[0].solutions.length).to eq(3)
+    end
+
   end
 
+  describe 'Formats' do
+    let(:api_instance) { A15kClient::FormatsApi.new }
+
+    it 'can be created' do
+      response = api_instance.create_format(
+        name: 'test', description: 'this is an explanation of the format'
+      )
+      expect(response.success).to eq(true)
+      expect(response.data).to be_a_kind_of(A15kClient::Format)
+      expect(response.data.as_json).to(
+        include(
+          'name' => 'test',
+          'description' => 'this is an explanation of the format'
+        )
+      )
+    end
+
+    it 'can be fetched' do
+      3.times{ FactoryBot.create(:format) }
+      response = api_instance.get_formats
+      expect(response.data).to be_a Array
+      expect(response.data.length).to be 3
+    end
+  end
 
 end

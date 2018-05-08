5f98cc73d5c115989883269c3ad843e6064bf7bf
Nathan Stitt <nathan@stitt.org>
2018-05-08 15:18:38 -0500

    full-stack test using ruby client

--- spec/clients/ruby.spec.rb
+++ spec/clients/ruby.spec.rb
@@ -1,43 +1,54 @@
 require_relative '../rails_helper'
 require 'a15k_client'
+require 'rack/test'
 
-describe 'Ruby client' do
+describe 'Ruby client', type: :api do
 
   let(:user) { FactoryBot.create(:user) }
+  let(:format) { FactoryBot.create(:format) }
   let(:api_instance) { A15kClient::AssessmentsApi.new }
-  # let(:headers) { { 'AUTHORIZATION' => Access::Token.for_user(user) } }
+  let(:authorization) { Access::Token.for_user(user) }
 
   before(:each) {
     A15kClient.configure do |c|
-      c.host = 'localhost:4001'
+      c.host = "localhost:#{api_server_port}"
       c.scheme = 'http'
-      # c.debugging = true
-      c.api_key['Authorization'] = ENV['A15K_AUTH']
+      c.api_key['Authorization'] = authorization
       c.verify_ssl_host = false
-      c.api_key_prefix['Authorization'] = 'Bearer'
     end
   }
 
   it 'can create an assessment' do
-      questions = exercise.delete('questions')
-
-      ox_format_uuid = "7ec91a74-ff17-4460-9776-1f2fc6b9ce61"
-
-      api_instance.api_v1_assessments_json_post(
-        format_id: ox_format_uuid,
-        content: JSON.generate(exercise)
-        questions: questions.map{|question|
-          answers = question.delete('answers')
-          {
-            format_id: ox_format_uuid,
-            content: JSON.generate(question)
-            solutions: answers.map{|answer|
-              format_id: ox_format_uuid,
-              content: JSON.generate(answer)
+    response = api_instance.create_assessment(
+      format_id: format.id,
+      identifier: 'TEST-Test-AND-TEST-MORE',
+      content: { text: Faker::Lorem.paragraph }.to_json,
+      questions: 2.times.map{|question_index|
+        {
+          format_id: format.id,
+          content: {
+            index: question_index,
+            text: Faker::Lorem.paragraph
+          }.to_json,
+          solutions: 3.times.map{ |solution_index|
+            {
+              format_id: format.id,
+              content: {
+                index: solution_index, text: Faker::Lorem.paragraph
+              }.to_json,
             }
           }
         }
-      )
+      }
+    )
+    expect(response.success).to eq(true)
+    expect(response.data).to be_a_kind_of(A15kClient::Assessment)
+    assessment = response.data
+    expect(assessment.identifier).to eq('TEST-Test-AND-TEST-MORE')
+    expect(assessment.format_id).to eq(format.id)
+    expect(assessment.questions.length).to eq(2)
+    expect(assessment.questions[0].solutions.length).to eq(3)
   end
+
 
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'rspec/rails'
+require_relative 'helpers/api_helper'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -26,23 +29,29 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.swagger_dry_run = false
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
 
-  config.before(:suite) do
+
+  config.before(:each) do
     DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before(:each, type: :api) do
+    Capybara.current_driver = :api
+    visit :api # force api to have the server booted and available
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 
   # RSpec Rails can automatically mix in different behaviours to your tests

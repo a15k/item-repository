93adbb0c4dad08374a2dccd3f31ec01682b8dcda
Nathan Stitt <nathan@stitt.org>
2018-05-09 15:24:43 -0500

    update to use better example with solution

--- spec/clients/kitchen-sink_spec.rb
+++ spec/clients/kitchen-sink_spec.rb
@@ -31,34 +31,28 @@ describe 'complete api flow', type: :api do
     assessments = A15kClient::AssessmentsApi.new
     assessment = assessments.create_assessment(
       format_id: format.id,
-      identifier: 'hippo-maddness', # this should be a UUID or something unique
+      identifier: 'driving-speed-distance', # must be a unique value
       questions: [
         {
           format_id: format.id,
-          content: <<~EOC
-             How much time do hippos spend in the water each day?
-
-             a) 2 hours
-             b) 16 hours
-             c) 24 hours
-
-             **answerkey:b**
-          EOC,
           solutions: [
-            {
-              format_id: format.id,
-              content: <<~EOC
-                a) that seems really low, hippos love water
-                b) excellent!
-                c) Do they really stay in the water all day long?
-              EOC
+            { format_id: format.id, content: "(60 mph)(0.5 hours) = 30 miles" }
+          ],
+          content: <<~EOC
+          A car drives at 60 mph for half an hour.  How far does it travel?
+
+          a) 120 miles
+          b) 30 miles
+          c) 60 miles
+
+          **answer: b**
+          EOC
         }
       ]
     ).data
 
     fetched = assessments.get_assessment(assessment.id).data
-    expect(fetched.questions[0].content).to include 'spend in the water'
-
+    expect(fetched.questions[0].content).to include 'drives at 60 mph'
   end
 
 end

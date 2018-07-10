8e5c422c87c1377ab833876c24631e099d97da1e
Nathan Stitt <nathan@stitt.org>
2018-07-10 15:29:38 -0500

    enforce power_user existence

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
 
   enum role: %i[power_user standard_user]
 
+  scope :power_users, -> { where(role: 'power_user') }
+
+  validate :membership_has_power_users
+
   def self.anonymous
     ::User::Anonymous.instance
   end
@@ -22,5 +26,18 @@ class User < ApplicationRecord
 
   def membership_access_token
      power_user? ? member.access_tokens.valid.first.token : nil
+  end
+
+  protected
+
+  def membership_has_power_users
+    if (member_id.nil? && member_id_changed?) || # unlinking
+       (role_changed? && !power_user?) # downgrading from power user
+
+      if Member.find(member_id || member_id_was).users.power_users.count < 2
+        errors.add(:member, 'must have at least one power user')
+      end
+
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe User, type: :model do
     user.update_attributes role: 'power_user'
     expect(user.membership_access_token).not_to be_nil
   end
+
+  it 'guards against last power user removal' do
+    member = FactoryBot.create :member
+    user1 = FactoryBot.create :user, member: member, role: 'power_user'
+    user2 = FactoryBot.create :user, member: member, role: 'power_user'
+    user1.update_attributes role: 'standard_user'
+    expect(user1.errors.none?).to be true
+
+    user2.update_attributes role: 'standard_user'
+    expect(user2.errors[:member]).to eq ['must have at least one power user']
+
+    user2.update_attributes role: 'power_user'
+    expect(user2.errors.none?).to be true
+    user2.update_attributes member: nil
+    expect(user2.errors[:member]).to eq ['must have at least one power user']
+  end
+
 end

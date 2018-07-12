1db6b6ab401500cbb8289ad989035979b1ef9ab1
Nathan Stitt <nathan@stitt.org>
2018-07-12 16:02:04 -0500

    auto-increment version, require member to be the same for versions

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -2,13 +2,31 @@
 class Assessment < ApplicationRecord
 
   belongs_to :format
-  belongs_to :created_by, class_name: 'Member'
+  belongs_to :member
+  has_many :questions, inverse_of: :assessment
+  has_many :other_versions,
+           -> (a) { where.not(id: a.id) },
+           class_name: 'Assessment',
+           foreign_key: :identifier, primary_key: :identifier
+
 
   enum visibility: %i[internal external]
 
-  has_many :questions, inverse_of: :assessment
-  validates :format, :created_by, presence: true
-
   validates :version, uniqueness: { scope: :identifier }
+
+  before_validation :set_version, on: :create
+  validate :ensure_member_matches_other_versions
+
+  protected
+
+  def set_version
+    self.version = other_versions.size + 1
+  end
+
+  def ensure_member_matches_other_versions
+    if other_versions.any? && member_id != other_versions.last.member_id
+      errors.add(:member, 'must be the same as other versions')
+    end
+  end
 
 end
--- spec/models/assessment_spec.rb
+++ spec/models/assessment_spec.rb
@@ -1,8 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Assessment, type: :model do
-  it "has working factory" do
-    asm = FactoryBot.create :assessment
-    expect(asm.questions.count).to eq 1
+  let(:member) { FactoryBot.create :member }
+  let(:assessment) { FactoryBot.create :assessment, member: member, identifier: '1234' }
+
+  it 'tracks versions' do
+    other = FactoryBot.create :assessment, member: member, identifier: '1234'
+    versions = assessment.other_versions.reload
+    expect(versions).not_to include assessment
+    expect(versions).to include other
   end
+
+  it 'disallows other members from making new versions' do
+    other_member = FactoryBot.create :member
+    other = Assessment.new(
+      identifier: '1234',
+      member: other_member,
+      format: assessment.format
+    )
+    expect(other.save).to be false
+    expect(other.errors[:member]).to include 'must be the same as other versions'
+  end
+
 end

b0a3dcfb98443099edec3c86370c1649ec1b0d71
Nathan Stitt <nathan@stitt.org>
2018-07-19 12:50:00 -0500

    export classnames

--- app/javascript/helpers/react.js
+++ app/javascript/helpers/react.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ModelCollection from '../models/model-collection';
+import cn from 'classnames';
 
 const ModelCollectionType = PropTypes.instanceOf(ModelCollection);
 
-export { React, PropTypes, ModelCollectionType };
+export { React, PropTypes, ModelCollectionType, cn };
 export { observable, action, computed } from 'mobx';
 export { observer } from 'mobx-react';

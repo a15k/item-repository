3f6f403fa3ff0fb7546e18851b9a52e27f5edce1
Nathan Stitt <nathan@stitt.org>
2018-07-19 12:50:09 -0500

    also set margin for surety-guard

--- app/javascript/components/buttons-bar.js
+++ app/javascript/components/buttons-bar.js
@@ -4,7 +4,10 @@ const ButtonsBar = styled.div`
 display: flex;
 margin: 1rem 0;
 justify-content: flex-end;
-.btn + .btn {
+.btn + .btn,
+.btn + .surety-guard,
+.surety-guard + .btn
+{
   margin-left: 1rem;
 }
 `;

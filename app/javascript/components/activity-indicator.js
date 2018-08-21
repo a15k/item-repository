a962a989578b6660d80b0dab439fc42ab9c6ce72
Nathan Stitt <nathan@stitt.org>
2018-08-20 22:47:56 -0600

    add an activity indicator component

---
+++ app/javascript/components/activity-indicator.js
@@ -0,0 +1,22 @@
+import { React, PropTypes } from '../helpers/react';
+import styled from 'styled-components';
+import Icon from './icon';
+
+const Indicator = styled.div`
+display: flex;
+align-items: center;
+.icon + span { margin-left: 0.5rem; }
+`;
+
+
+export default function ActivityIndicator({
+  icon = 'spinner',
+  label = 'Loading…',
+}) {
+  return (
+    <Indicator>
+      {icon && <Icon icon={icon} spin />}
+      <span>{label}</span>
+    </Indicator>
+  );
+}

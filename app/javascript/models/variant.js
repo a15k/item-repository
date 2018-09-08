a0b7ffd80bc5dd3b1e2cc316e9400b2695500e40
JP Slavinsky <jpslav@gmail.com>
2018-09-08 14:45:55 -0600

    add fields to JS models for better download schema

--- app/javascript/models/variant.js
+++ app/javascript/models/variant.js
@@ -1,15 +1,16 @@
 import {
-  BaseModel, identifiedBy, field, identifier, hasMany,
+  BaseModel, identifiedBy, field, identifier,
 } from './base';
-import Solution from './solution';
+// import Solution from './solution';
 
 @identifiedBy('variant')
 export default class Variant extends BaseModel {
 
   @identifier id;
+  @field source_identifier;
   @field format_id;
   @field({ type: 'object' }) content;
   @field preview_html;
-  @hasMany({ model: Solution }) solutions;
+  // @hasMany({ model: Solution }) solutions;
 
 }
